fe6df8787b52dd1848512c1683dfc7d4f0af99b8
Shubham Mishra1 <shubham_mishra1@epam.com>
2022-03-23 10:51:13 +0530

    new files added

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
 
     Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+    Stream<Integer> scoreStream= Arrays.stream(scores);
 
     List<String> shoppingList = new ArrayList<>();
     shoppingList.add("coffee");
@@ -15,6 +20,12 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+    Stream<String> shoppingListStream=shoppingList.stream();
+    shoppingListStream.sorted().forEach((item)-> System.out.println(item));
+
+    System.out.println(shoppingList.stream().map((item)->item.toUpperCase()).collect(Collectors.toList()));
+
+    System.out.println(shoppingList.stream().map((item)->item.toUpperCase()).filter((item)->item.startsWith("P")).collect(Collectors.toList()));
 
   }
 }
